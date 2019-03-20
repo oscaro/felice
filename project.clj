3a4f4266a6ccf62f3f071eed676b6e54e8f69df9
Mathieu <mathieu.marchandise@oscaro.com>
2019-03-20 16:24:10 +0100

    add oscaro repositories project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject com.oscaro/felice "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :repositories [["oscaro-releases"  {:url "https://artifactory.oscaroad.com/artifactory/libs-release-local"}]
+                 ["oscaro-snapshots" {:url "https://artifactory.oscaroad.com/artifactory/libs-snapshot-local"}]
+                 ["oscaro-remote"    {:url "https://artifactory.oscaroad.com/artifactory/remote-repos"}]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
